f8acb6055c6ef958bdb5beb9683f341a35cee698
bakaruyu <50687616+bakaruyu@users.noreply.github.com>
2022-09-08 20:27:48 -0500

    Update Message.java

--- Message.java
+++ Message.java
@@ -2,6 +2,6 @@ public class Message
 {
     public static void main( String[] args )
     {
-        System.out.println( "Este es un mensaje de bienvenida" );
+        System.out.println( "Este es un mensaje de salida" );
     }
 }
